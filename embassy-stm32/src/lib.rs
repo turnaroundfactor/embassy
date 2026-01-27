fca5edc2ec8c5bf19148ab0627895a39e3e00fa4
jake-taf <149392739+jake-taf@users.noreply.github.com>
2026-01-27 17:18:22 -0500

    Update lib.rs

--- embassy-stm32/src/lib.rs
+++ embassy-stm32/src/lib.rs
@@ -527,162 +527,162 @@ fn init_hw(config: Config) -> Peripherals {
         {
     debug!("init - DB02");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
-            crate::pac::RCC.miscensr().write(|w| w.set_dbgens(true));
+            // crate::pac::RCC.miscensr().write(|w| w.set_dbgens(true));
     debug!("init - DB03");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
-            crate::pac::RCC.miscenr().read(); // volatile read
+            // crate::pac::RCC.miscenr().read(); // volatile read
     debug!("init - DB04");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
-            crate::pac::DBGMCU
-                .cr()
-                .modify(|w| w.set_dbgclken(stm32_metapac::dbgmcu::vals::Dbgclken::B_0X1));
+            // crate::pac::DBGMCU
+            //     .cr()
+            //     .modify(|w| w.set_dbgclken(stm32_metapac::dbgmcu::vals::Dbgclken::B_0X1));
     debug!("init - DB05");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
-            crate::pac::DBGMCU.cr().read();
+            // crate::pac::DBGMCU.cr().read();
     debug!("init - DB06");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
         }
 
     debug!("init - DB07");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
-        #[cfg(dbgmcu)]
-        crate::pac::DBGMCU.cr().modify(|cr| {
+        // #[cfg(dbgmcu)]
+        // crate::pac::DBGMCU.cr().modify(|cr| {
     debug!("init - DB08");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
-            #[cfg(dbgmcu_h5)]
-            {
-                cr.set_stop(config.enable_debug_during_sleep);
-                cr.set_standby(config.enable_debug_during_sleep);
-            }
-            #[cfg(any(
-                dbgmcu_f0, dbgmcu_c0, dbgmcu_g0, dbgmcu_u0, dbgmcu_u3, dbgmcu_u5, dbgmcu_wba, dbgmcu_l5
-            ))]
-            {
-                cr.set_dbg_stop(config.enable_debug_during_sleep);
-                cr.set_dbg_standby(config.enable_debug_during_sleep);
-            }
+            // #[cfg(dbgmcu_h5)]
+            // {
+            //     cr.set_stop(config.enable_debug_during_sleep);
+            //     cr.set_standby(config.enable_debug_during_sleep);
+            // }
+            // #[cfg(any(
+            //     dbgmcu_f0, dbgmcu_c0, dbgmcu_g0, dbgmcu_u0, dbgmcu_u3, dbgmcu_u5, dbgmcu_wba, dbgmcu_l5
+            // ))]
+            // {
+            //     cr.set_dbg_stop(config.enable_debug_during_sleep);
+            //     cr.set_dbg_standby(config.enable_debug_during_sleep);
+            // }
     debug!("init - DB09");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
-            #[cfg(any(
-                dbgmcu_f1, dbgmcu_f2, dbgmcu_f3, dbgmcu_f4, dbgmcu_f7, dbgmcu_g4, dbgmcu_f7, dbgmcu_l0, dbgmcu_l1,
-                dbgmcu_l4, dbgmcu_wb, dbgmcu_wl, dbgmcu_n6
-            ))]
-            {
-                cr.set_dbg_sleep(config.enable_debug_during_sleep);
-                cr.set_dbg_stop(config.enable_debug_during_sleep);
-                cr.set_dbg_standby(config.enable_debug_during_sleep);
-            }
+            // #[cfg(any(
+            //     dbgmcu_f1, dbgmcu_f2, dbgmcu_f3, dbgmcu_f4, dbgmcu_f7, dbgmcu_g4, dbgmcu_f7, dbgmcu_l0, dbgmcu_l1,
+            //     dbgmcu_l4, dbgmcu_wb, dbgmcu_wl, dbgmcu_n6
+            // ))]
+            // {
+            //     cr.set_dbg_sleep(config.enable_debug_during_sleep);
+            //     cr.set_dbg_stop(config.enable_debug_during_sleep);
+            //     cr.set_dbg_standby(config.enable_debug_during_sleep);
+            // }
     debug!("init - DB10");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
-            #[cfg(dbgmcu_h7)]
-            {
-                cr.set_d1dbgcken(config.enable_debug_during_sleep);
-                cr.set_d3dbgcken(config.enable_debug_during_sleep);
-                cr.set_dbgsleep_d1(config.enable_debug_during_sleep);
-                cr.set_dbgstby_d1(config.enable_debug_during_sleep);
-                cr.set_dbgstop_d1(config.enable_debug_during_sleep);
-            }
-        });
+        //     #[cfg(dbgmcu_h7)]
+        //     {
+        //         cr.set_d1dbgcken(config.enable_debug_during_sleep);
+        //         cr.set_d3dbgcken(config.enable_debug_during_sleep);
+        //         cr.set_dbgsleep_d1(config.enable_debug_during_sleep);
+        //         cr.set_dbgstby_d1(config.enable_debug_during_sleep);
+        //         cr.set_dbgstop_d1(config.enable_debug_during_sleep);
+        //     }
+        // });
     debug!("init - DB11");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
 
     debug!("init - DB12");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
-        #[cfg(not(any(stm32f1, stm32wb, stm32wl, stm32h7rs)))]
-        rcc::enable_and_reset_with_cs::<peripherals::SYSCFG>(cs);
+        // #[cfg(not(any(stm32f1, stm32wb, stm32wl, stm32h7rs)))]
+        // rcc::enable_and_reset_with_cs::<peripherals::SYSCFG>(cs);
     debug!("init - DB13");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
-        #[cfg(not(any(stm32h5, stm32h7, stm32h7rs, stm32wb, stm32wl)))]
-        rcc::enable_and_reset_with_cs::<peripherals::PWR>(cs);
+        // #[cfg(not(any(stm32h5, stm32h7, stm32h7rs, stm32wb, stm32wl)))]
+        // rcc::enable_and_reset_with_cs::<peripherals::PWR>(cs);
     debug!("init - DB14");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
-        #[cfg(all(flash, not(any(stm32f2, stm32f4, stm32f7, stm32l0, stm32h5, stm32h7, stm32h7rs))))]
-        rcc::enable_and_reset_with_cs::<peripherals::FLASH>(cs);
+        // #[cfg(all(flash, not(any(stm32f2, stm32f4, stm32f7, stm32l0, stm32h5, stm32h7, stm32h7rs))))]
+        // rcc::enable_and_reset_with_cs::<peripherals::FLASH>(cs);
     debug!("init - DB15");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
 
         // Enable the VDDIO2 power supply on chips that have it.
         // Note that this requires the PWR peripheral to be enabled first.
-        #[cfg(any(stm32l4, stm32l5))]
-        {
-            crate::pac::PWR.cr2().modify(|w| {
-                // The official documentation states that we should ideally enable VDDIO2
-                // through the PVME2 bit, but it looks like this isn't required,
-                // and CubeMX itself skips this step.
-                w.set_iosv(config.enable_independent_io_supply);
-            });
-        }
-        #[cfg(stm32wba)]
-        {
-            use crate::pac::pwr::vals;
-            crate::pac::PWR.svmcr().modify(|w| {
-                w.set_io2sv(if config.enable_independent_io_supply {
-                    vals::Io2sv::B_0X1
-                } else {
-                    vals::Io2sv::B_0X0
-                });
-            });
-        }
-        #[cfg(any(stm32u5, stm32u3))]
-        {
-            crate::pac::PWR.svmcr().modify(|w| {
-                w.set_io2sv(config.enable_independent_io_supply);
-            });
-            if config.enable_independent_analog_supply {
-                crate::pac::PWR.svmcr().modify(|w| {
-                    w.set_avm1en(true);
-                });
-                while !crate::pac::PWR.svmsr().read().vdda1rdy() {}
-                crate::pac::PWR.svmcr().modify(|w| {
-                    w.set_asv(true);
-                });
-            } else {
-                crate::pac::PWR.svmcr().modify(|w| {
-                    w.set_avm1en(false);
-                    w.set_avm2en(false);
-                });
-            }
-        }
+        // #[cfg(any(stm32l4, stm32l5))]
+        // {
+        //     crate::pac::PWR.cr2().modify(|w| {
+        //         // The official documentation states that we should ideally enable VDDIO2
+        //         // through the PVME2 bit, but it looks like this isn't required,
+        //         // and CubeMX itself skips this step.
+        //         w.set_iosv(config.enable_independent_io_supply);
+        //     });
+        // }
+        // #[cfg(stm32wba)]
+        // {
+        //     use crate::pac::pwr::vals;
+        //     crate::pac::PWR.svmcr().modify(|w| {
+        //         w.set_io2sv(if config.enable_independent_io_supply {
+        //             vals::Io2sv::B_0X1
+        //         } else {
+        //             vals::Io2sv::B_0X0
+        //         });
+        //     });
+        // }
+        // #[cfg(any(stm32u5, stm32u3))]
+        // {
+        //     crate::pac::PWR.svmcr().modify(|w| {
+        //         w.set_io2sv(config.enable_independent_io_supply);
+        //     });
+        //     if config.enable_independent_analog_supply {
+        //         crate::pac::PWR.svmcr().modify(|w| {
+        //             w.set_avm1en(true);
+        //         });
+        //         while !crate::pac::PWR.svmsr().read().vdda1rdy() {}
+        //         crate::pac::PWR.svmcr().modify(|w| {
+        //             w.set_asv(true);
+        //         });
+        //     } else {
+        //         crate::pac::PWR.svmcr().modify(|w| {
+        //             w.set_avm1en(false);
+        //             w.set_avm2en(false);
+        //         });
+        //     }
+        // }
 
         // dead battery functionality is still present on these
         // chips despite them not having UCPD- disable it
-        #[cfg(any(stm32g070, stm32g0b0))]
-        {
-            crate::pac::SYSCFG.cfgr1().modify(|w| {
-                w.set_ucpd1_strobe(true);
-                w.set_ucpd2_strobe(true);
-            });
-        }
+        // #[cfg(any(stm32g070, stm32g0b0))]
+        // {
+        //     crate::pac::SYSCFG.cfgr1().modify(|w| {
+        //         w.set_ucpd1_strobe(true);
+        //         w.set_ucpd2_strobe(true);
+        //     });
+        // }
 
     debug!("init - DB20");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
         unsafe {
-            #[cfg(ucpd)]
-            ucpd::init(
-                cs,
-                #[cfg(all(peri_ucpd1, not(stm32n6)))]
-                config.enable_ucpd1_dead_battery,
-                #[cfg(peri_ucpd2)]
-                config.enable_ucpd2_dead_battery,
-            );
+            // #[cfg(ucpd)]
+            // ucpd::init(
+            //     cs,
+            //     #[cfg(all(peri_ucpd1, not(stm32n6)))]
+            //     config.enable_ucpd1_dead_battery,
+            //     #[cfg(peri_ucpd2)]
+            //     config.enable_ucpd2_dead_battery,
+            // );
     debug!("init - DB21");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
 
-            #[cfg(feature = "_split-pins-enabled")]
-            crate::pac::SYSCFG.pmcr().modify(|pmcr| {
-                #[cfg(feature = "split-pa0")]
-                pmcr.set_pa0so(true);
-                #[cfg(feature = "split-pa1")]
-                pmcr.set_pa1so(true);
-                #[cfg(feature = "split-pc2")]
-                pmcr.set_pc2so(true);
-                #[cfg(feature = "split-pc3")]
-                pmcr.set_pc3so(true);
-            });
+            // #[cfg(feature = "_split-pins-enabled")]
+            // crate::pac::SYSCFG.pmcr().modify(|pmcr| {
+            //     #[cfg(feature = "split-pa0")]
+            //     pmcr.set_pa0so(true);
+            //     #[cfg(feature = "split-pa1")]
+            //     pmcr.set_pa1so(true);
+            //     #[cfg(feature = "split-pc2")]
+            //     pmcr.set_pc2so(true);
+            //     #[cfg(feature = "split-pc3")]
+            //     pmcr.set_pc3so(true);
+            // });
 
     debug!("init - DB22");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
-            gpio::init(cs);
+            // gpio::init(cs);
     debug!("init - DB23");
     for _ in 0..2_000_000 {cortex_m::asm::nop()};
             // dma::init(
