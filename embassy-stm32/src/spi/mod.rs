ae9708f32d314326d84c033832a8acf8d708c4eb
jake-taf <149392739+jake-taf@users.noreply.github.com>
2026-06-17 10:03:24 +0100

    Check EOT, fence

--- embassy-stm32/src/spi/mod.rs
+++ embassy-stm32/src/spi/mod.rs
@@ -611,9 +611,12 @@ impl<'d, M: PeriMode, CM: CommunicationMode> Spi<'d, M, CM> {
 
         transfer_words(self.info.regs, words, &[])?;
 
+        fence(Ordering::SeqCst);
         // Wait until transfer is actually complete before returning
         #[cfg(not(any(spi_v1, spi_v2, spi_v3)))]
         while !self.info.regs.sr().read().txc() {}
+        #[cfg(not(any(spi_v1, spi_v2, spi_v3)))]
+        while !self.info.regs.sr().read().eot() {}
         #[cfg(spi_v3)]
         while self.info.regs.sr().read().bsy() {}
         Ok(())
