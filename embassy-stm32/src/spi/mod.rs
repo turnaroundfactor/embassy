27470157a6c6b26edb8df7d10048c2efb118c01c
jake-taf <149392739+jake-taf@users.noreply.github.com>
2026-06-17 11:44:20 +0100

    tsize

--- embassy-stm32/src/spi/mod.rs
+++ embassy-stm32/src/spi/mod.rs
@@ -605,7 +605,10 @@ impl<'d, M: PeriMode, CM: CommunicationMode> Spi<'d, M, CM> {
         self.set_word_size(W::CONFIG);
         self.info.regs.cr1().modify(|w| w.set_spe(true));
         flush_rx_fifo(self.info.regs);
-
+        
+        self.info.regs.cr2().modify(|w| {
+            w.set_tsize(words.len() as u16);
+        });
         // Memory barrier after flush RX fifo to ensure register writes complete
         fence(Ordering::SeqCst);
 
