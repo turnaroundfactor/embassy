a7e382e0acad28643706415cff5102dee966b343
jake-taf <jake@turnaroundfactor.com>
2026-06-28 23:48:43 +0100

    Formatting

--- embassy-stm32/src/adc/mod.rs
+++ embassy-stm32/src/adc/mod.rs
@@ -1007,7 +1007,9 @@ pub(crate) trait AnalogPin {
 
 impl<T: crate::gpio::SealedPin> AnalogPin for T {
     fn set_as_analog(&self) {
-        #[cfg(any(adc_v1, adc_c0, adc_l0, adc_v2, adc_g4, adc_v3, adc_v4, adc_u3, adc_u5, adc_wba, stm32n6))]
+        #[cfg(any(
+            adc_v1, adc_c0, adc_l0, adc_v2, adc_g4, adc_v3, adc_v4, adc_u3, adc_u5, adc_wba, stm32n6
+        ))]
         T::set_as_analog(self);
     }
 }
